9bcc0f2e2b173bef66d448309cd6aed3da7d8697
Bolgov Alexandr Sergeevich <138039571+BolgovAlex@users.noreply.github.com>
2024-01-15 18:39:22 +0300

    you've seen 100 dramas but never been to the theater

--- 26-1.py
+++ 26-1.py
@@ -1,6 +1,7 @@
 f = open('26-1.txt')
 amount, budget = map(int, f.readline().split())
 ans1 = 0
+j=0
 bars = []
 for _ in range(amount):
     taste, price, quantity = f.readline().split()
@@ -8,21 +9,28 @@ for _ in range(amount):
 f.close()
 bars.sort(key=lambda x: (x[1]))
 for i in range(amount):
-    if budget - bars[i][1] * bars[i][2] < 0:
-        break
-    if bars[i][0] == 'O' and budget > 0:
+    if bars[i][0] == 'O':
+        if budget - bars[i][1] * bars[i][2] < 0:
+            j=i
+            break
         budget -= bars[i][1] * bars[i][2]
 for i in range(amount):
-    if budget - bars[i][1] * bars[i][2] < 0:
-        break
     if bars[i][0] == 'F':
+        if budget - bars[i][1] * bars[i][2] < 0:
+            j=i
+            break
         ans1 += bars[i][2]
         budget -= bars[i][1] * bars[i][2]
 for i in range(amount):
-    if budget - bars[i][1] * bars[i][2] < 0:
-        break
     if bars[i][0] == 'K':
+        if budget - bars[i][1] * bars[i][2] < 0:
+            j=i
+            break
         budget -= bars[i][1] * bars[i][2]
-while budget - bars[i][1] > 0:
-    budget -= bars[i][1]
+while budget - bars[j][1] > 0:
+    if bars[j][2] > 0:
+        budget -= bars[j][1]
+        bars[j][2] -= 1
+    if bars[j][2] <= 0:
+        j+=1
 print(ans1, budget)
